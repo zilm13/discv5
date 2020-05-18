52e0f6ef91ade4d2b97b64bb0b9b27f6e65aacd8
Dmitrii Shmatko <leodex23@gmail.com>
2020-05-18 11:36:02 +0300

    Add churn rate in router

--- src/main/kotlin/org/ethereum/discv5/core/Router.kt
+++ src/main/kotlin/org/ethereum/discv5/core/Router.kt
@@ -2,10 +2,16 @@ package org.ethereum.discv5.core
 
 import io.libp2p.core.PeerId
 import org.apache.logging.log4j.LogManager
+import java.util.Random
 
-class Router {
+class Router(private val rnd: Random) {
     private val idToNode: MutableMap<PeerId, Node> = HashMap()
     private val logger = LogManager.getLogger("Router")
+    var churnPcts = 0
+        set(value) {
+            if (value > 100 || value < 0) error("Value should be from 0 to 100 percents")
+            field = value
+        }
 
     fun register(node: Node) {
         idToNode.putIfAbsent(node.enr.id, node)
@@ -19,9 +25,6 @@ class Router {
         return resolve(enr.id)
     }
 
-    /**
-     * TODO: real down nodes and network losses
-     */
     fun route(from: Node, to: Enr, message: Message): List<Message> {
         from.outgoingMessages.add(message.getSize().toLong())
         from.roundtripLatency.add(listOf(Unit))
@@ -29,6 +32,12 @@ class Router {
         if (toNode == null) {
             logger.debug("Failed to route $message from Node${from.enr.toId()} to Node${to.toId()}, recipient not found")
             return emptyList()
+        }
+        // Apply churn rate
+        if (churnPcts > 0) {
+            if (rnd.nextInt(100) < churnPcts) {
+                return emptyList()
+            }
         }
         toNode.incomingMessages.add(message.getSize().toLong())
         return toNode.handle(message, from.enr).also { messages ->
